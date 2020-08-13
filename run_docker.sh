6066f4104cac4b4f588eae9fb914313e9af654a3
Balakrishnan R <59216990+balavpy@users.noreply.github.com>
2020-08-13 18:38:13 +0530

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag udacity_bala:latest
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker container run -dt --name udacity --port 80:80 udacity_bala:latest
